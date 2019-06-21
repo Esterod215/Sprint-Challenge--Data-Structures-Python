75df12d1f369c0284ead1b0e390e2c72bc5a8b76
Estevan <estevan.rodriguez10@yahoo.com>
2019-06-21 09:04:32 -0700

    first pass solution for RINGBUFFER

--- ring_buffer/ring_buffer.py
+++ ring_buffer/ring_buffer.py
@@ -5,7 +5,22 @@ class RingBuffer:
     self.storage = [None]*capacity
 
   def append(self, item):
-    pass
+    newest = 0
+    oldest = 0
+    if len(self.storage) < capacity:
+      self.storage.append(item)
+      newest = self.storage.index(item)
+    else:
+      self.storage[oldest] = value 
+      newest = self.storage.index(value)
+      if oldest + 1 > len(self.storage):
+        oldest = newest
+      else:
+        oldest = oldest + 1
+        
 
   def get(self):
     return self.storage
+
+
+    
